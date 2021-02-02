204963e147d9c8d50e481b8a6851d91461898cc1
Jair Nagano <rafaeljnagano@MacBook-Air-de-Jair.local>
2021-02-02 02:28:37 -0600

    Fix: Formulario de contacto

---
+++ js/slider.js
@@ -0,0 +1,40 @@
+$(document).ready(function ($) {
+
+    var jssor_1_options = {
+      $AutoPlay: 1,
+      $DragOrientation: 2,
+      $PlayOrientation: 2,
+      $BulletNavigatorOptions: {
+        $Class: $JssorBulletNavigator$,
+        $Orientation: 2
+      }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*#region responsive code begin*/
+
+    var MAX_WIDTH = 1080;
+
+    function ScaleSlider() {
+        var containerElement = jssor_1_slider.$Elmt.parentNode;
+        var containerWidth = containerElement.clientWidth;
+
+        if (containerWidth) {
+
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+            jssor_1_slider.$ScaleWidth(expectedWidth);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 60);
+        }
+    }
+
+    ScaleSlider();
+
+    $Jssor$.$AddEvent(window, "load", ScaleSlider);
+    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+    /*#endregion responsive code end*/
+});
